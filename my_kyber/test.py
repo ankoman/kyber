fd81439e57a60b9ca6311e9e036c3415a2775ce5
ankoman <i08320.wctsonhb@gmail.com>
2025-06-26 00:13:38 +0900

    Add capacity

--- my_kyber/test.py
+++ my_kyber/test.py
@@ -346,7 +346,20 @@ def main():
             print(seed, hex(m), hex(d), rot, scalar, wrong_bits, hw_mp)
 
 def h2(p):
+    p = min(max(p, 1e-12), 1 - 1e-12)
     return -p*math.log2(p)-(1-p)*math.log2(1-p)
+
+def clip(x):
+    if x < 0.5:
+        return x
+    else:
+        return 0.5
+
+def h(p,q):
+    p = min(max(p, 1e-12), 1 - 1e-12)
+    q = min(max(q, 1e-12), 1 - 1e-12)
+
+    return -p*math.log2(p)-q*math.log2(q)
 
 if __name__ == '__main__':
     # prob()
@@ -357,25 +370,39 @@ if __name__ == '__main__':
 
     from scipy.stats import norm
 
-    sum_p = 0
+    p11 = 0
+    p00 = 0
+    p0 = 0
+
     b_range = 0
-    sigma_b = 51
-    M_r = 150
-    n_d = 100
+    sigma_b = 79
+    M_r = 256
+    n_d = 256
     tau = 2*M_r//n_d
     list_i = list(range(-n_d//2, n_d//2+1))
     d = max(list_i)
+    c = -d*tau-10
+
     for i in list_i:
-        p = norm.cdf(d*tau, i*tau, sigma_b)
-        print(p)
-        if p > 0.0:
-            sum_p += p
+        p11 += clip(1 - norm.cdf(-c, i*tau, sigma_b))
+        p00 += clip(norm.cdf(-c, i*tau, sigma_b))
+        p0 += norm.cdf(-c, i*tau, sigma_b)
     
-    sum_p /= (n_d + 1)
-    print(f'{sum_p=}')
-    sum_p = 1 - sum_p
-    print(f'{sum_p=}')
-    print(-sum_p*math.log2(sum_p))
+    p11 /= (n_d + 1)
+    p00 /= (n_d + 1)
+    p0 /= (n_d + 1)
+    p1 = 1 - p0
+    p10 = p1 - p11
+    p01 = p0 - p00
+
+    print(f'{p11=}, {p10=}, {p01=}, {p00=}, {p0=}, {p1=}')
+
+    H_XY = p0*h(p00,p01) + p1*h(p10,p11)
+    H_X = 1
+    C = H_X - H_XY
+    print(f'{H_XY=}, {H_X=}, {C=}')
+
+
 
 
 
